d19c0ae4ec70b0b7d1007a89c6acfed298f158e8
Patrick Moon <patrickmoon@outlook.com>
2021-04-29 16:22:03 -0700

    improving the basic systems.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,60 +4,16 @@
             {{ __('Phase1 Game') }}
         </h2>
     </x-slot>
-
-    <style>
-        .offline {
-            background-color: #2d3748;
-        }
-        .totals {
-            font-family: sans-serif, Tahoma;
-            font-size: 8pt;
-        }
-
-        .total_amounts, .levels  {
-            padding: .25em;
-            margin: .25em;
-            border: 1px solid black;
-
-            min-width: 8em;
-        }
-        .levels {
-            min-width: 20em;
-            min-height: 10em;
-        }
-        .total_amounts {
-            float: left;
-        }
-        .gather {
-            float: right;
-        }
-        .gather_improve {
-            float: left;
-            clear: both;
-        }
-        .red {
-            background-color: darkred;
-        }
-        .green {
-            background-color: green;
-        }
-    </style>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @livewire('total-resources')
+    <div class="p-4 grid grid-cols-12">
+        <div class="col-span-12">
+            @livewire('total-resources')
+        </div>
+        <div class="col-span-12">
+            <div class="p-4 grid grid-cols-3 gap-4">
+                @livewire('gather', ['resourceId' => 1, 'resourceName' => 'Clay', 'enabled' => 1, 'allowed' => 1, 'canEnable' => 1])
+                @livewire('gather', ['resourceId' => 2, 'resourceName' => 'Water', 'enabled' => 0, 'allowed' => 1, 'canEnable' => 0])
+                @livewire('gather', ['resourceId' => 3, 'resourceName' => 'Wood', 'enabled' => 0, 'allowed' => 1, 'canEnable' => 0])
             </div>
-        </div>
-    </div>
-    <div class="py-12">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8"  id="level1">
-            @livewire('gather', ['resourceId' => 1, 'resourceName' => 'Clay', 'enabled' => 1, 'allowed' => 1, 'canEnable' => 1])
-        </div>
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8"  id="level2">
-            @livewire('gather', ['resourceId' => 2, 'resourceName' => 'Water', 'enabled' => 0, 'allowed' => 1, 'canEnable' => 0])
-        </div>
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8"  id="level2">
-            @livewire('gather', ['id' => 3, 'name' => 'Wood', 'enabled' => 0, 'allowed' => 0, 'canEnable' => 0])
         </div>
     </div>
 </x-app-layout>
--- resources/views/livewire/gather.blade.php
+++ resources/views/livewire/gather.blade.php
@@ -1,18 +1,23 @@
 @if($allowed)
-    <div class="{{($canEnable) ? '' : 'offline '}}levels bg-white overflow-hidden shadow-xl sm:rounded-lg">
-        <h1>{{ $resourceName }}: <span class="{{ ($allowed) ? 'green' : 'red' }}">Allowed</span> _
-            <span class="{{ ($canEnable) ? 'green' : 'red' }}">Can Enable</span> _
-            <span class="{{ ($enabled) ? 'green' : 'red' }}">Enabled</span>
-        </h1>
-        <div class="gather">
-            <div wire:click="gather" class="{{($enabled) ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-500 hover:bg-gray-700'}} text-white font-bold py-2 px-4 rounded-full">Gather <span class="gather_amount_to_add">{{ $gatherAmount }}</span></div>
+    <div id="level{{ $resourceId }}" class="text-center p-4 overflow-hidden shadow-xl {{($canEnable) ? 'bg-blue-200' : 'bg-gray-900'}} rounded-b-lg ">
+        <div class="title">
+            <h2 class="p-1 font-semibold text-xl text-left {{($canEnable) ? 'text-black' : 'text-white'}} leading-tight">{{ $resourceName }}</h2>
+            <div class="p-2 grid grid-cols-6 gap-2 sm:font-mono text-xs">
+                <span class="rounded-full {{ ($allowed) ? 'bg-green-500 hover:bg-green-300' : 'bg-red-500 hover:bg-red-300' }}">Allowed</span>
+                <span class="rounded-full {{ ($canEnable) ? 'bg-green-500 hover:bg-green-300' : 'bg-red-500 hover:bg-red-300' }}">Can Enable</span>
+                <span class="rounded-full {{ ($enabled) ? 'bg-green-500 hover:bg-green-300' : 'bg-red-500 hover:bg-red-300' }}">Enabled</span>
+                <span class="rounded-full {{ ($automated) ? 'bg-green-500 hover:bg-green-300' : 'bg-red-500 hover:bg-red-300' }}">Automated</span>
+                <span class="rounded-full {{ ($canImprove) ? 'bg-green-500 hover:bg-green-300' : 'bg-red-500 hover:bg-red-300' }}">Can Improve</span>
+            </div>
         </div>
-        <div class="gather_improve">
-            <div wire:click="improveGather" class="improveGather {{($enabled) ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-500 hover:bg-gray-700'}} text-white font-bold py-2 px-4 rounded-full">Improve</div>
-        </div>
-        <div class="activateType">
-            @if($canEnable && ! $enabled)
-                <div wire:click="enable" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Enable</div>
+        <div class="body p-2 grid grid-cols-6 gap-2  text-white font-bold">
+            <div wire:click="gather" class="gather {{($enabled) ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-500 hover:bg-gray-700'}} rounded-full">Gather <span class="gather_amount_to_add">{{ $gatherAmount }}</span></div>
+            <div wire:click="improve" class="gather_improve text-center {{($enabled && $canImprove) ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-500 hover:bg-gray-700'}} rounded-full">Improve</div>
+            @if(! $enabled)
+                <div wire:click="enable" class="{{ ($canEnable) ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-500 hover:bg-gray-700' }} rounded-full">Enable</div>
+            @endif
+            @if(! $automated)
+                <div wire:click="automate" class="{{ ($canAutomate) ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-500 hover:bg-gray-700' }} rounded-full">Automate</div>
             @endif
         </div>
     </div>
